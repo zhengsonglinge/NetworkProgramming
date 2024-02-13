958d05be793fcba3cfd9b074820e8a67d32ed8d5
zhengsonglinge <zhengsonglinge@163.com>
2024-02-13 20:33:04 +0800

    windows function

---
+++ chapter3/windows/conv_addr_win.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <winsock2.h>
+
+int main(int argc, char const *argv[])
+{
+    char *strAddr = "203.211.218.102:9190";
+
+    char strAddrBuf[50];
+    SOCKADDR_IN servAddr;
+    int size;
+
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    size = sizeof(servAddr);
+    WSAStringToAddress(
+        strAddr, AF_INET, NULL, (SOCKADDR *)&servAddr, &size);
+
+    size = sizeof(strAddrBuf);
+    WSAAddressToString(
+        (SOCKADDR *)&servAddr, sizeof(servAddr), NULL, strAddrBuf, &size);
+
+    printf("Second conv result: %s \n", strAddrBuf);
+
+    WSACleanup();
+    return 0;
+}
---
+++ chapter3/windows/inet_adrconv_win.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+void ErrorHandling(char *message);
+
+int main(int argc, char const *argv[])
+{
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+        ErrorHandling("WSAStartup() error!");
+
+    // inet_addr 函数
+    {
+        char *addr = "127.0212.124.78";
+        unsigned long conv_addr = inet_addr(addr);
+        if (conv_addr == INADDR_NONE)
+            printf("Error occured! \n");
+        else
+            printf("Network ordered integer addr: %#lx \n", conv_addr);
+    }
+    // inet_ntoa 函数
+    {
+        struct sockaddr_in addr;
+        char *strPtr;
+        char strArr[20];
+
+        addr.sin_addr.s_addr = htonl(0x1020304);
+        strPtr = inet_ntoa(addr.sin_addr);
+        strcpy(strArr, strPtr);
+        printf("Dotted-Decimal notation3 %s \n", strArr);
+    }
+
+    WSACleanup();
+    return 0;
+}
+
+void ErrorHandling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
